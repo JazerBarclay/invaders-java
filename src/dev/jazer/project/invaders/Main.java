1d8e04c1fafd35cb628a83c0a0f34495712002e9
Jazer <contact@jazer.co.uk>
2021-04-14 12:54:43 +0100

    Created main class to test javafx

---
+++ src/dev/jazer/project/invaders/Main.java
@@ -0,0 +1,22 @@
+package dev.jazer.project.invaders;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,800,400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
