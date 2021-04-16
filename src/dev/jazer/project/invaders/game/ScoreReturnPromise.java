5379ddb24f0bf1ac083dbccea782581c5dd692a5
Jazer <contact@jazer.co.uk>
2021-04-16 12:11:39 +0100

    Added documentation

--- src/dev/jazer/project/invaders/game/ScoreReturnPromise.java
+++ src/dev/jazer/project/invaders/game/ScoreReturnPromise.java
@@ -1,5 +1,9 @@
 package dev.jazer.project.invaders.game;
 
+/**
+ * An interface to perform an action when the score value is returned
+ * @author Jazer Barclay
+ */
 public interface ScoreReturnPromise {
 
 	public void onReturn(int value);
