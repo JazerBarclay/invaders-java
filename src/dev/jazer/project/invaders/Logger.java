89e33c3f731ed3db60fc3d5d0ff66311c97c6b10
Jazer <contact@jazer.co.uk>
2021-04-22 14:24:01 +0100

    Added documentation

--- src/dev/jazer/project/invaders/Logger.java
+++ src/dev/jazer/project/invaders/Logger.java
@@ -3,12 +3,22 @@ package dev.jazer.project.invaders;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Simple synchronised logging for info, warning and error terminal messages
+ * @author Jazer Barclay
+ *
+ */
 public class Logger {
-
-	public Logger() {
-		
-	}
 	
+	/**
+	 * Prints to the console the current time, parent class, message and exception<br><br>
+	 * If an exception is defined, the output will be set in red with syserr
+	 * 
+	 * @param type
+	 * @param parent
+	 * @param message
+	 * @param e
+	 */
 	private static void print(String type, Object parent, String message, Exception e) {
 		String text = new SimpleDateFormat("HH:mm:ss (SSS)").format(new Date()) + " : " + type + " : " + parent.getClass().getSimpleName() + " : " + message;
 		if (e == null) {
